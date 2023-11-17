1b95b3e27710746636bf2f4591d8c79c27c83e19
Wegooooo <wagdym014@gmail.com>
2023-11-17 22:30:39 +0200

    commit

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ def monitor(domain, path):
     # enumerate subdomains
     proccess = Popen(
         f"amass enum -active -passive -brute -d {domain} -w /home/Kalawy/Pentest/SecLists/Discovery/DNS/subdomains-top1million-110000.txt >> {path}/newsubdomains", shell=True,stdout=PIPE)
+    proccesses.append(proccess)
     proccess.wait()
-
+    
+    proccesses.remove(proccess)
+    
     # delete outofscope
     with open(f"{path}/.config/outofscope", "r") as outofscope_file:
         with open(f"{path}/newsubdomains", "r") as newsubdomains_file:
@@ -15,10 +18,18 @@ def monitor(domain, path):
                 if subdomain in outofscope:
                     newsubdomains.remove(subdomain)
             anew = Popen(f"cat {path}/newsubdomains | anew {path}/subdomains >> {path}/new")
+            proccesses.append(anew)
             anew.wait()
-                
+            proccesses.remove(anew)
+            
+proccesses = []
+            
 with open("/home/Kalawy/.config/monitor/targets") as targets_file:
-    targets = targets_file.readlines()
-    for target in targets:
-        target = target.split()
-        monitor(target[0], target[1])
+    try:
+        targets = targets_file.readlines()
+        for target in targets:
+            target = target.split()
+            monitor(target[0], target[1])
+    except KeyboardInterrupt:
+        for pro in proccesses:
+            pro.kill()
