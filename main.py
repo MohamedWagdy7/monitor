939b51e086f278fd7b05caf933f66332277c90e7
Wegooooo <wagdym014@gmail.com>
2023-11-18 22:28:29 +0200

    c

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from subprocess import Popen,PIPE
 def monitor(domain, path):
     # enumerate subdomains
     proccess = Popen(
-        f"amass enum -active -passive -brute -d {domain} -w /home/Kalawy/Pentest/SecLists/Discovery/DNS/subdomains-top1million-110000.txt >> {path}/newsubdomains", shell=True,stdout=PIPE)
+        f"amass enum -active -passive -brute -d {domain} -w /home/Kalawy/Pentest/SecLists/Discovery/DNS/subdomains-top1million-110000.txt > {path}/newsubdomains", shell=True,stdout=PIPE)
     proccess.wait()    
     # delete outofscope
     with open(f"{path}/.config/outofscope", "r") as outofscope_file:
@@ -14,7 +14,7 @@ def monitor(domain, path):
             for subdomain in newsubdomains:
                 if subdomain in outofscope:
                     newsubdomains.remove(subdomain)
-            anew = Popen(f"cat {path}/newsubdomains | anew {path}/subdomains >> {path}/new",shell=True)
+            anew = Popen(f"cat {path}/newsubdomains | anew {path}/subdomains > {path}/new",shell=True)
             anew.wait()
 
 with open("/home/Kalawy/.config/monitor/targets") as targets_file:
